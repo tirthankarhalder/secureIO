381848beb0d64598e246965c76d4f81ca1043254
Tirthankar-Halder <tirthankarhalder70@gmail.com>
2024-10-16 18:38:17 +0530

    first commit

---
+++ esp32s3Cam/app_httpd.cpp
@@ -0,0 +1,6 @@
+// static int run_face_recognition(fb_data_t *fb, std::list<dl::detect::result_t> *results)
+// static esp_err_t bmp_handler(httpd_req_t *req)
+// static size_t jpg_encode_stream(void *arg, size_t index, const void *data, size_t len)
+// static esp_err_t capture_handler(httpd_req_t *req)
+// static esp_err_t stream_handler(httpd_req_t *req)
+// void startCameraServer()
---
+++ esp32s3Cam/img_converters.h
@@ -0,0 +1,6 @@
+// bool fmt2jpg_cb(uint8_t *src, size_t src_len, uint16_t width, uint16_t height, pixformat_t format, uint8_t quality, jpg_out_cb cb, void * arg);
+// bool frame2jpg_cb(camera_fb_t * fb, uint8_t quality, jpg_out_cb cb, void * arg);
+// bool fmt2jpg(uint8_t *src, size_t src_len, uint16_t width, uint16_t height, pixformat_t format, uint8_t quality, uint8_t ** out, size_t * out_len);
+// bool frame2jpg(camera_fb_t * fb, uint8_t quality, uint8_t ** out, size_t * out_len);
+// bool fmt2bmp(uint8_t *src, size_t src_len, uint16_t width, uint16_t height, pixformat_t format, uint8_t ** out, size_t * out_len);
+// bool frame2bmp(camera_fb_t * fb, uint8_t ** out, size_t * out_len);
